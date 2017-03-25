0fbcc32bcd2c1466eb6b294d1a8a0eb9734350fa
五十嵐翔 <igara1119@gmail.com>
2017-03-25 17:23:13 +0900

    weatherコマンドの削除

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const hears = {
 	help: "help",
 	release: "release",
 	commit_log: "commit_log",
-	weather: "weather",
 };
 
 const path = require("path");
@@ -98,35 +97,4 @@ controller.hears(hears.commit_log + " " + release.self, "direct_mention",(bot, m
 		}
 	};
 	bot.startConversation(message, ask_commit_log);
-});
-
-/**
- * @robohon weather を実行した時にサンプルのお答えをする
- */
-controller.hears(hears.weather, "direct_mention", function(bot,message) {
-	console.log(message);
-	const askWeather = function(response, convo) {
-		console.log(response);
-		convo.ask("明日は雨です。車で出かけられますか？", function(response, convo) {
-			console.log(response);
-			convo.say("承知しました。");
-			if(response.text == "yes"){
-				askCar(response, convo);
-			}
-			convo.next();
-		});
-	};
-
-	const askCar = function(response, convo) {
-		convo.ask("折りたたみ傘が必要ですか？", function(response, convo) {
-			if(response.text == "yes") {
-				convo.say("承知いたしました。ご用意いたします。");
-			} else {
-				convo.say("お気をつけてください。");
-			}
-			convo.next();
-		});
-	};
-
-	bot.startConversation(message, askWeather);
 });
